e190d795d8fa61bccb7ab140c2522035fba8410d
ARIF515 <81582653+ARIF515@users.noreply.github.com>
2021-07-19 19:53:34 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Laptop Store ID</title>
+    <link rel="stylesheet" href="style1.css" />
+</head>
+<body>
+    <div id="header">
+        <center><h1>Menambahkan Data</h1></center>
+        <br><br>
+    </div>
